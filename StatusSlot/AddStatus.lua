70472a6a59506ec0033fa99b22ce7d1f3c3b24c8
Garantae <91921033+Garantae@users.noreply.github.com>
2021-10-05 17:22:25 -0400

    Update AddStatus.lua

--- StatusSlot/AddStatus.lua
+++ StatusSlot/AddStatus.lua
@@ -1,10 +1,23 @@
+--[[
+
+	AddStatus
+	@author Garantae
+
+	Controls the appearance of the text as well as the request for the removal of the text
+	after its set amount of visibility time.
+
+--]]
+
+--// Configurables
+local StatusDuration = 2 -- How long the text will be visible on-screen
+
 --// Fade text in
 while script.Parent.TextTransparency > 0 do
 	script.Parent.TextTransparency = script.Parent.TextTransparency - 0.1
 	wait(0.01)
 end
 
-wait(2)
+wait(StatusDuration)
 
 --// Fade text out
 while script.Parent.TextTransparency < 1 do
